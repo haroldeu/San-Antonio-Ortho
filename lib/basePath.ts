5fe375a2f5a90a972db97cfa9cfede1529d4219d
haroldeu <haroldeuu@gmail.com>
2026-01-30 17:19:44 +0800

    fix: Fix image issue in vercel deployment

--- lib/basePath.ts
+++ lib/basePath.ts
@@ -1,5 +1,1 @@
-const repoBasePath = "/San-Antonio-Ortho";
-
-export const basePath =
-  process.env.NEXT_PUBLIC_BASE_PATH ??
-  (process.env.NODE_ENV === "production" ? repoBasePath : "");
+export const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
